eb783a9ca3b15d00f8d77f7e1496ec066bc67d3d
Patryk Grudzień <patryk.grudzien.91@gmail.com>
2023-03-22 15:54:02 +0100

    Initialization of a `student-api` module

---
+++ student-api/src/test/java/me/kodey/StudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kodey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
