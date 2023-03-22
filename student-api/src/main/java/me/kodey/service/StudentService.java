b4c151d6e374add571296993bea5664c6dddadad
Patryk Grudzień <patryk.grudzien.91@gmail.com>
2023-03-22 16:49:46 +0100

    Make usage of a new Spring feature `ObservationRegistry`

--- student-api/src/main/java/me/kodey/service/StudentService.java
+++ student-api/src/main/java/me/kodey/service/StudentService.java
@@ -1,5 +1,7 @@
 package me.kodey.service;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import me.kodey.model.Student;
 import org.springframework.stereotype.Service;
@@ -13,16 +15,30 @@ public class StudentService {
 
     private static final List<Student> STUDENTS = new ArrayList<>();
 
+    private final ObservationRegistry observationRegistry;
+
     public Student addStudent(Student student) {
         STUDENTS.add(student);
-        return student;
+        // return student;
+        return Observation
+                .createNotStarted("addStudent", observationRegistry)
+                .observe(() -> student);
     }
 
     public List<Student> getAllStudents() {
-        return STUDENTS;
+        // return STUDENTS;
+        return Observation
+                .createNotStarted("getAllStudents", observationRegistry)
+                .observe(() -> STUDENTS);
     }
 
     public Student getStudent(String id) {
+        return Observation
+                .createNotStarted("getStudent", observationRegistry)
+                .observe(() -> getStudentOrThrow(id));
+    }
+
+    private static Student getStudentOrThrow(String id) {
         return STUDENTS
                 .stream()
                 .filter(student -> id.equals(student.id()))
